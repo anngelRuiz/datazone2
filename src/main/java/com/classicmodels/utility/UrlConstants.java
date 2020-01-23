f3fd86790dd5b2d5cce0a3be62ae4f3f1d0c8f6e
Armando Vazquez Ruiz <anngelruiz@hotmail.com>
2020-01-23 15:06:02 -0700

    Primer commit

---
+++ src/main/java/com/classicmodels/utility/UrlConstants.java
@@ -0,0 +1,16 @@
+package com.classicmodels.utility;
+
+public class UrlConstants {
+
+    // Controllers URL's
+    public static final String INDEX_URL = "";
+    public static final String CUSTOMER_URL = "/customer";
+
+
+    // Restcontrollers URL's
+    public static final String LIST_ALL = "/list";
+    public static final String GET_BY_ID = "/get/{id}";
+    public static final String SAVE = "/save";
+    public static final String UPDATE = "/update/{id}";
+    public static final String DELETE_BY_ID = "/delete/{id}";
+}
